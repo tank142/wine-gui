86584172713d63e707d66c6f9c3fa97d9f43c032
tank142 <tank.142@protonmail.com>
2021-03-15 21:56:42 +0400

    Update sizewin.cpp

--- src/sizewin.cpp
+++ src/sizewin.cpp
@@ -14,23 +14,20 @@ sizeWin::~sizeWin(){
 void sizeWin::restore(){
 	if(!settings_conf->contains("main/" + name)){return;}
 	QList size = settings_conf->value("main/" + name).toList();
-	if(size.size() > 4){
-		switch ( size.at(4).toInt() ) {
-		case 0:
-			win->move(size.at(0).toInt(),
-					  size.at(1).toInt());
-			win->resize(size.at(2).toInt(),
-						size.at(3).toInt());
-		break;
-		case 2:
-			win->setWindowState(Qt::WindowState::WindowMaximized);
-		break;
-		case 8:
-			win->setWindowState(Qt::WindowState::WindowFullScreen);
-		break;
-		}
+	win->move(size.at(0).toInt(),
+			  size.at(1).toInt());
+	win->resize(size.at(2).toInt(),
+				size.at(3).toInt());
+	switch ( size.at(4).toInt() ) {
+	case 0:
+	break;
+	case 2:
+		win->setWindowState(Qt::WindowState::WindowMaximized);
+	break;
+	case 8:
+		win->setWindowState(Qt::WindowState::WindowFullScreen);
+	break;
 	}
-	cout << size.at(4).toInt() << endl;
 }
 void sizeWin::save(){
 	settings_conf->setValue("main/" + name,QVariant(QStringList()
